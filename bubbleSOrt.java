8523239b98bdb844bb720650fba901dd3244a0ed
Bhargavyerragolla <Bhargavsai8887@gmail.com>
2023-09-06 15:10:22 +0530

    changed the array count

--- bubbleSOrt.java
+++ bubbleSOrt.java
@@ -8,7 +8,7 @@ public class bubbleSOrt {
     }
   
     public static void main(String args[]) {
-        int arr[] = {7, 8, 1, 3, 2};
+        int arr[] = {7, 8, 1, 3, 2,3,7};
   
         //bubble sort
         for(int i=0; i<arr.length; i++) {
