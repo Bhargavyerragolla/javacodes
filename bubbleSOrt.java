caa2af1acae96f7666c1dfc1003d11543a25ab2a
Bhargavyerragolla <Bhargavsai8887@gmail.com>
2023-09-12 16:30:46 +0530

    added subraction

--- bubbleSOrt.java
+++ bubbleSOrt.java
@@ -1,10 +1,8 @@
 public class bubbleSOrt {
 	 
-    public static void printArray(int arr[]) {
-        for(int i=0; i<arr.length; i++) {
-            System.out.print(arr[i]+" ");
-        }
-        System.out.println();
+    public static void printArray(int a,int b) {
+      
+        System.out.println(a-b);
     }
 
 
@@ -13,21 +11,8 @@ public class bubbleSOrt {
     }
   
     public static void main(String args[]) {
-        int arr[] = {7, 8, 1, 3, 2,3,7};
-  
-        //bubble sort
-        for(int i=0; i<arr.length; i++) {
-            for(int j=0; j<arr.length-i-1; j++) {
-                if(arr[j] >arr[j+1]) {
-                    //swap
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-            }
-        }
-  
-        printArray(arr);
+        
+  printArray(10, 4);
         bubbleSOrt bOrt= new bubbleSOrt();
         bOrt.task1();
         
